6899e501e15bfe7194fabaeb2b499d0239d7686c
hukumka <hukumka212@gmail.com>
2019-02-20 23:16:04 +0000

    Extend doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+/// Experimenting with building capnp message transport
+/// With following considerations in mind:
+/// + Avoid messages splitting (https://github.com/capnproto/capnproto-rust/issues/93)
+/// + If possible, achive prevoius goal without copying.
+/// + capnp-futures implementation of reads and writes is reactor unaware, but it should be. Is it
+/// though? #TODO: prove or disprove.
+///
+/// First problem is ideomaticlly fixed by wrapping stream inside WriteBuf.
+/// But it brings need to manage flushing, and it is not copy free.
+///
+/// Question: is it ok to flush after every message send?
+///
+/// Other solution is to allocate first segment with some extra space before
+/// actual segment. Then, if it large enough to fit segment table
+/// write segment table to it's end, and then concatenate slices
+/// via little of unsafe.
+///
+/// Third problem requires implementing it over AsyncWrite and AsyncRead, which either
+/// mean depending on tokio, or adding such behaviour after futures stabilization.
+/// Latter is preferable IMO.
 #[macro_use]
 extern crate log;
 
